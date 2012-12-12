d32d1e7827d27c50a698388759321d61bfbeef54
rohan <rohanmathur34@gmail.com>
2012-12-11 19:19:16 -0500

    Fix a few graphical glitches

--- msm8960.mk
+++ msm8960.mk
@@ -141,9 +141,9 @@ PRODUCT_COPY_FILES += \
 # Common build properties
 PRODUCT_PROPERTY_OVERRIDES += \
     com.qc.hardware=true \
-    debug.composition.type=gpu \
+    debug.composition.type=dyn \
     debug.egl.hw=1 \
-    debug.mdpcomp.maxlayer=3 \
+    debug.mdpcomp.maxlayer=0 \
     debug.mdpcomp.logs=0 \
     debug.sf.hw=1 \
     dev.pm.dyn_samplingrate=1 \
