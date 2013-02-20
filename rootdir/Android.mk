2e22bb8d4651718438446b4eb5067f3f7f6c05be
Gergely Szell <sethyx@gmail.com>
2013-02-20 17:20:09 +0100

    Additional board flag checks in rootdir
    Change-Id: Id97e9d130c67210a1156ac0402f9cf0619c134f8
    Signed-off-by: Gergely Szell <sethyx@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,5 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
+ifeq ($(BOARD_VENDOR),htc)
+ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.qcom.sh
 LOCAL_MODULE_TAGS	:= optional eng
@@ -31,3 +33,5 @@ LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/ueventd.qcom.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+endif
+endif
