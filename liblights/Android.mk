eaabe668e513c554309a92bf1db194c04ae7bf92
Whitehawkx <xxwhitehawk@gmail.com>
2013-11-28 21:01:45 -0800

    msm8960: update liblights and power define
    Change-Id: I9944df993f203a1fdfe6ebf583a554252c5d87f6
    Signed-off-by: Whitehawkx <xxwhitehawk@gmail.com>

--- liblights/Android.mk
+++ liblights/Android.mk
@@ -14,7 +14,7 @@
 
 LOCAL_PATH:= $(call my-dir)
 
-ifeq ($(TARGET_PROVIDES_LIBLIGHT),true)
+ifeq ($(USE_DEVICE_SPECIFIC_LIBLIGHT),true)
 ifeq ($(BOARD_VENDOR),htc)
 ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
 
