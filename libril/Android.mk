368f8d5d22e2e8d9eb1c8aa194ec9a476cc02beb
Matt Mower <mowerm@gmail.com>
2017-01-21 00:27:05 -0600

    msm8960-common: libril cleanup
    * Bring our imported libril into closer alignment with
      hardware/ril-caf/libril, including:
      - move 'include' headers to an include directory, matching the
        directory structure of hardware/ril-caf/include
      - allow for imperfect code formatting (makes future diffs easier)
    * Re-add logging of custom HTC signal strength struct
    
    libril should be diff'ed with: hardware/ril-caf
      ril-caf: Only allow GET_CARRIER_RESTRICTIONS for ril versions >= 14
      commit 5291cf85725431964f9817a71d1fb0234dc0b09d
    
    Change-Id: I1bca470b17178747788f36ac0cc5bbe21a2564b1

--- libril/Android.mk
+++ libril/Android.mk
@@ -10,7 +10,7 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES:= \
     ril.cpp \
-    ril_event.cpp \
+    ril_event.cpp\
     RilSocket.cpp \
     RilSapSocket.cpp \
 
@@ -20,7 +20,7 @@ LOCAL_SHARED_LIBRARIES := \
     libbinder \
     libcutils \
     libhardware_legacy \
-    librilutils
+    librilutils \
 
 LOCAL_STATIC_LIBRARIES := \
     libprotobuf-c-nano-enable_malloc \
@@ -35,15 +35,12 @@ ifeq ($(BOARD_RIL_FIVE_SEARCH_RESPONSES),true)
     LOCAL_CFLAGS += -DRIL_FIVE_SEARCH_RESPONSES
 endif
 
-LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/librilutils
 LOCAL_C_INCLUDES += external/nanopb-c
-
-LOCAL_CLANG := false
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/../include
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/../include
 
 LOCAL_MODULE:= libril
-
-LOCAL_COPY_HEADERS_TO := libril
-LOCAL_COPY_HEADERS := ril_ex.h
+LOCAL_CLANG := false
 
 include $(BUILD_SHARED_LIBRARY)
 
--- libril/RilSapSocket.cpp
+++ libril/RilSapSocket.cpp
@@ -86,7 +86,7 @@ void RilSapSocket::sOnUnsolicitedResponse(int unsolResponse,
        const void *data,
        size_t datalen) {
     RilSapSocket *sap_socket = getSocketById(RIL_SOCKET_1);
-    if (sap_socket) {
+    if(sap_socket){
         sap_socket->onUnsolicitedResponse(unsolResponse, (void *)data, datalen);
     }
 }
