2c7d72fef7ee4090ca6ac0e461fdbe454da38f56
Lakshman Chaluvaraju <lchalu@codeaurora.org>
2018-07-26 15:04:37 +0530

    msm8960-common: Move Audio HAL from 2.0 to 4.0
    - Moving Audio HAL from 2.0 to 4.0.
    
    Change-Id: I623d92bae679a89b0af4863d315f34edaf30019f

--- msm8960-common.mk
+++ msm8960-common.mk
@@ -45,7 +45,7 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
+    android.hardware.audio@4.0-impl \
     android.hardware.audio.effect@2.0-impl \
     audio.a2dp.default \
     audio.primary.msm8960 \
