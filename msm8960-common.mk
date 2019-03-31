19bd4ebaa54e1febd4138758f081700bcae9c1a8
TARKZiM <tom8476oo@gmail.com>
2019-03-31 20:57:02 +0800

    msm8960-common: Convert lights HAL to native binderized HAL
    * from https://github.com/LineageOS/android_device_htc_msm8974-common
    
    Change-Id: I5383bbd4c0dca3e59ca38db413af2c4a0b804fad

--- msm8960-common.mk
+++ msm8960-common.mk
@@ -69,8 +69,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    lights.msm8960
+    android.hardware.light@2.0-service.htc_msm8960
 
 # Media
 PRODUCT_COPY_FILES += \
