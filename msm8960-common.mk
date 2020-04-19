619c23813f27bf173f0719ac048406fa27234b73
Julian Veit <claymore1298@gmail.com>
2020-04-19 21:47:54 +0200

    msm8960-common: reorganize configs
    Change-Id: Ic2a81906ed4324cda9d406966701b53cebe8ea37

--- msm8960-common.mk
+++ msm8960-common.mk
@@ -75,7 +75,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_codecs_google_video.xml \
-    $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_codecs.xml
+    $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/media_codecs.xml
 
 # OMX
 PRODUCT_PACKAGES += \
