6111e01b37bffabc2c7d0237aa952f2a5a61d26c
Julian Veit <claymore1298@gmail.com>
2020-04-22 19:00:42 +0200

    msm8960-common: build audio 2.0 and effects
    Change-Id: If444339197525ab4f45bc3cc309adbd3d7bff345

--- msm8960-common.mk
+++ msm8960-common.mk
@@ -41,6 +41,8 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
     audio.a2dp.default \
     audio.primary.msm8960 \
     audio.r_submix.default \
