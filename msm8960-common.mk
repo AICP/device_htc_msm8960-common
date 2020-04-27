1b40afe7d79878d0441d5e18cd074125eb8cc257
Julian Veit <claymore1298@gmail.com>
2020-04-28 00:38:06 +0200

    m7-common: build binderized drm hal
    Change-Id: I684f711af923ee669612fe78330488fc80c78516

--- msm8960-common.mk
+++ msm8960-common.mk
@@ -72,7 +72,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
 
 # JamesDSPManager
 PRODUCT_PACKAGES += \
