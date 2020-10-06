679f6a5670c698134cac9b61fa1b297fb96a422b
althafvly <althafvly@gmail.com>
2020-10-06 19:49:54 +0530

    msm8960-common: Switch to Android Go builds
    Recommended for 2gb or less ram devices
    https://www.android.com/versions/go-edition/
    
    Change-Id: I4c58d95c3b54c4e00179962a8dc8feb5ec3019b5

--- msm8960-common.mk
+++ msm8960-common.mk
@@ -19,6 +19,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/go_defaults.mk)
 
 # Permissions
 PRODUCT_COPY_FILES += \
